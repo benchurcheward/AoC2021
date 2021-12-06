f0d8f9027a71ab67268bdfaedb1fb8ab878c42e1
bchrchwd <benjamin.churcheward@etu.univ-nantes.fr>
2021-12-06 01:55:24 +0100

    Updated : more pythonic

--- Day3/day3.py
+++ Day3/day3.py
@@ -5,49 +5,30 @@ import os, sys
 
 def gamma_epsilon(list_bits):
     """
-    Iterate through a list of lines of bits.
     Computes gamma rate and epsilon rate, encoding as lines of bits:
     Bits of gamma are the most common bits at each position.
     Bits of espilon are the least common bits at each position.
 
     Returns product of gamma and epsilon.
-    O(Nk+2k), with:
-    N = number of binary lines in list_bits
-    k = number of bits in one binary line
     """
 
     n = len(list_bits)
+    k = len(list_bits[0])-1
     counts  = [0]*len(list_bits[0])
-    gamma   = [0]*len(list_bits[0])
-    epsilon = [0]*len(list_bits[0])
-    gamma_rate = 0
-    epsilon_rate = 0
+    gamma = 0
+    epsilon = 0
 
-    for binary in list_bits:
-        #binary = binary.split()
-        for i in range(len(binary)):
-            counts[i] += binary[i]
+    counts = [sum(bit) for bit in zip(*list_bits)]
 
     for i in range(len(counts)):
-        #counts[i] represent the number of 1 at ith position
-        #if > n/2, then 1 is more common
-        #put sup or equal as there is no clue to manage equalities
+        #most common = 1
         if counts[i] >= n/2:
-            gamma[i] = 1
-            epsilon[i] = 0
+            gamma += 1 << k-i
+        #most common = 0
         else:
-            gamma[i] = 0
-            epsilon[i] = 1
-    for i in range(len(gamma)):
-        #in python list index increase from left to right
-        #so we need to inverse position for the decimal conversion
-        if gamma[len(gamma)-i-1] == 1:
-            #** is the power operator
-            gamma_rate += 2**i
-        if epsilon[len(gamma)-i-1] == 1:
-            epsilon_rate += 2**i
+            epsilon += 1 << k-i
 
-    return gamma_rate * epsilon_rate
+    return gamma * epsilon
 
 def life_support(list_bits):
     """
@@ -60,76 +41,45 @@ def life_support(list_bits):
     Same, but with least common bit at each position.
     """
 
-    n = len(list_bits)
-    count      = 0
+    count    = 0
     o2_bits  = [bits for bits in list_bits]
     co2_bits = [bits for bits in list_bits]
-    o2_rating  = 0
-    co2_rating = 0
-    i = 0
+    o2  = 0
+    co2 = 0
+    i  = 0
+    n1 = len(o2_bits)
+    n2 = len(co2_bits)
+    n  = len(list_bits[0]) - 1
 
-    #I followed linearly the text of the exercise, but note that a single loop
-    #is possible, saving time
+    while n1 > 1 or n2 > 1:
+        count_o2  = sum([bit[i] for bit in o2_bits])
+        count_co2 = sum([bit[i] for bit in co2_bits])
 
-    #compute o2_binary
-    while n > 1:
-        count = 0
-        for binary in o2_bits:
-            count += binary[i]
-        #in case of equality, choose 1 for the most common,
-        #0 for the last common
-        if count >= n/2:
-            most_common = 1
+        if n1 == 1:
+            pass
+        #in case of equality, choose 1 for the most common
+        elif count_o2 >= n1/2:
+            #most common = 1 ; least common = 0
+            o2  += 1 << n-i
+            o2_bits  = [bits for bits in o2_bits if bits[i] == 1]
         else:
-            most_common = 0
-
-        #(re)construct the o2_bits list
-        #keeping only the bits with the most common i-th bit
-        o2_bits = [bits for bits in o2_bits if bits[i] == most_common]
+            #most common = 0 ; least common = 1
+            o2_bits  = [bits for bits in o2_bits if bits[i] == 0]
+            
+        if n2 == 1:
+            pass
+        #in case of equality, choose 0 for the least common
+        elif count_co2 < n2/2:
+            co2  += 1 << n-i
+            co2_bits = [bits for bits in co2_bits if bits[i] == 1]
+        else:
+            co2_bits = [bits for bits in co2_bits if bits[i] == 0]
         #update values
-        n = len(o2_bits)
+        n1 = len(o2_bits)
+        n2 = len(co2_bits)
         i += 1
 
-    #re-initiate n as original number of binary lines
-    n = len(list_bits)
-    i = 0
-
-    #compute co2_binary
-    while n > 1:
-        count = 0
-        for binary in co2_bits:
-            count += binary[i]
-        #in case of equalities, choose 0
-        if count >= n/2:
-            least_common = 0
-        else:
-            least_common = 1
-
-        #(re)construct the co2_bits list
-        #keeping only the bits with the least common i-th bit
-        co2_bits = [bits for bits in co2_bits if bits[i] == least_common]
-        #update values
-        n = len(co2_bits)
-        i += 1
-
-    o2_bits  = o2_bits[0]
-    co2_bits = co2_bits[0]
-    n = len(o2_bits)
-
-    #convert into decimals
-    for i in range(n):
-        if o2_bits[n-i-1] == 1:
-            o2_rating += 2**i
-        if co2_bits[n-i-1] == 1:
-            co2_rating += 2**i
-
-    ##note that the lines beyond do exactly the same thing
-    ##using python list comprehension
-    ##int(str, n) convert str to int following a base n
-    #o2_rating = int(''.join([str(bit) for bit in o2_bits]), 2)
-    #co2_rating = int(''.join([str(bit) for bit in co2_bits]), 2)
-
-    life_support_rating = o2_rating * co2_rating
+    life_support_rating = o2 * co2
 
     return life_support_rating
 
