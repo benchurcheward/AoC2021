5841c53d7aaa813fddcdfe18267daed037cab8c0
bchrchwd <benj.churcheward@gmail.com>
2021-12-07 19:14:17 +0100

    Simplified puzzle2, removed obsolete lines

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,57 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os, sys
+import sys
 
 #test = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-
-def sliding_window(input_file, width):
-    """
-    Avoid to load the whole file into a list to save memory.
-    Width is customizable, but the exercise needs width = 3.
-    """
-    with open(input_file, 'r') as f:
-        count = 0
-        #return the next element of iterator f
-        #split removes the endline character '\n'
-        first  = int(next(f).strip())
-        second = int(next(f).strip())
-        third  = int(next(f).strip())
-        prevdepth = first + second + third
-
-        while True:
-            try:
-                #upload the values
-                #as it is a sliding window, only the third is new
-                #and needs to be read from f
-                first  = second
-                second = third
-                third  = int(next(f).strip())
-
-                depth = first + second + third
-
-                if depth > prevdepth:
-                    count += 1
-                prevdepth = depth
-
-            except StopIteration:
-                #will trigger after reaching the last element
-                break
-    return count
 
 def main():
 
     input_file = sys.argv[1]
     with open(input_file) as f:
         m = f.readlines()
+        #Puzzle1
         count_increase = sum([1 for (m1, m2) in zip(m, m[1:]) if m2 > m1])
+        #Puzzle2
+        count_sliding = sum([1 for (m1,m2,m3,m4) in zip(measures, m[1:], m[2:], m[3:]) if sum((m2,m3,m4)) > sum((m1,m2,m3))])
 
     print(f'There are {count_increase} measurements larger than the precedent.')
-    #count_increase = read_depths_2(input_file)
-    #print(f'There are {count_increase} measurements larger than the precedent.')
-    count_sliding = sliding_window(input_file, 3)
-    print(f'There are {count_sliding} measurements larger than the precedent.')
-    return
+    print(f'There are {count_sliding} triplets larger than the precedent.')
 
 if __name__ == '__main__':
     main()
