17056debadd83234277a45562bcaa5f0277bffda
bchrchwd <benj.churcheward@gmail.com>
2021-12-07 14:02:22 +0100

    Simplified first exercise

--- Day1/day1.py
+++ Day1/day1.py
@@ -5,48 +5,6 @@ import os, sys
 
 #test = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
-def read_depths(input_file):
-    """
-    Simple: read the file, iterates through the list of lines.
-    """
-    with open(input_file) as f:
-        count = 0
-        #file lines are str
-        #if we don't change them into int, we'd miss one increase
-        previous = int(f.readline())
-        #splitlines removes the endline character '\n'
-        depths = f.read().splitlines()
-        for depth in depths:
-            depth = int(depth)
-            if depth > previous:
-                count += 1
-            else:
-                pass
-            #upload the previous value
-            previous = depth
-    return count
-
-def read_depths_2(input_file):
-    """
-    Avoid to load the whole file into a list to save memory.
-    """
-    with open(input_file) as f:
-        count = 0
-        #return the next element of iterator f
-        #split removes the endline character '\n'
-        previous = int(next(f).strip())
-        while True:
-            try:
-                depth = int(next(f).strip())
-                if depth > previous:
-                    count += 1
-
-                previous = depth
-            except StopIteration:
-                #will trigger after reaching the last element
-                break
-    return count
-
 def sliding_window(input_file, width):
     """
     Avoid to load the whole file into a list to save memory.
@@ -84,14 +42,15 @@ def sliding_window(input_file, width):
 def main():
 
     input_file = sys.argv[1]
+    with open(input_file) as f:
+        m = f.readlines()
+        count_increase = sum([1 for (m1, m2) in zip(m, m[1:]) if m2 > m1])
 
-    count_increase = read_depths(input_file)
-
     print(f'There are {count_increase} measurements larger than the precedent.')
-    count_increase = read_depths_2(input_file)
-    print(f'There are {count_increase} measurements larger than the precedent.')
-    count_increase = sliding_window(input_file, 3)
-    print(f'There are {count_increase} measurements larger than the precedent.')
+    #count_increase = read_depths_2(input_file)
+    #print(f'There are {count_increase} measurements larger than the precedent.')
+    count_sliding = sliding_window(input_file, 3)
+    print(f'There are {count_sliding} measurements larger than the precedent.')
     return
 
 if __name__ == '__main__':
