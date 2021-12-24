aac9b0788eec56b50ba53ef0e70c5e86b8d42ed7
bchrchwd <benj.churcheward@gmail.com>
2021-12-24 15:54:37 +0100

    Cleaned code

--- Day8/day8.py
+++ Day8/day8.py
@@ -27,93 +27,65 @@ def main():
                     digits[8] += 1
     print(sum(digits))
 
-    #puzzle2
-    #dico = {'a':'', 'b':'', 'c':'', 'd':'', 'e':'', 'f':'', 'g':''}
-    #digits = ['']*10
-    #for i in range(10):
-    #    dico[i] = []
     with open(input_file) as f:
         sum_output = 0
         while(line := next(f, None)) is not None:
             unknown = []
             digits = [set() for i in range(10)]
-            #print(line)
             line = line.split(' | ')
             signal = line[0].split(' ')
             output = line[1].strip().split(' ')
-            #print(signal)
-            #print('###############')
-            #print(output)
+
             for word in signal:
-                #print(word)
                 word = set([l for l in word])
-                #print(word)
                 if len(word) == 2:
                     digits[1] = word
-                    #print('1 found !')
                 elif len(word) == 3:
                     digits[7] = word
-                    #print('7 found !')
                 elif len(word) == 4:
                     digits[4] = word
-                    #print('4 found !')
                 elif len(word) == 5:
                     unknown.append(word)
                 elif len(word) == 6:
                     unknown.append(word)
                 elif len(word) == 7:
                     digits[8] = word
-                    #print('8 found !')
             while True:
                 unknown = [word for word in unknown if word not in digits]
-                #print(unknown)
                 if not unknown:
-                    #print('Terminated !')
                     break
                 for word in unknown:
                     if len(word) == 5:
-                        if digits[1].issubset(word):
-                            #print('3 found !')
+                        if digits[1] <= (word):
                             digits[3] = word
                         else:
                             if digits[6]:
-                                #print(digits[6])
                                 if len(word & digits[1] & digits[6]) == 1:
                                         digits[5] = word
-                                        #print('5 found !')
                                         continue
                                 else:
                                     digits[2] = word
-                                    #print('2 found !')
                                     continue
                             else:
                                 continue
                     if len(word) == 6:
                         if digits[4] <= word:
                             digits[9] = word
-                            #print('9 found !')
                             continue
                         elif digits[7] <= word:
                             digits[0] = word
-                            #print('0 found !')
                             continue
                         else:
                             digits[6] = word
-                            #print(digits[6])
-                            #print('6 found !')
                             continue
-            #digits = [''.join(list(word)) for word in digits]
-            #print(digits)
-            #sum_output = 0
             number = ''
-            #print(output)
             output = [set([l for l in word]) for word in output]
             for word in output:
                 if word in digits:
                     number = f"{number}{digits.index(word)}"
             print(f"Number is {number}")
             sum_output += int(number)
-            print(f"Sum of numbers are : {sum_output}")
+        print(f"Sum of numbers are : {sum_output}")
 
 if __name__ == '__main__':
     main()
