083e0d72ea21ed686cfc0cf34e8568ee4cc43f70
bchrchwd <benj.churcheward@gmail.com>
2021-12-24 02:28:23 +0100

    Added day7#2

--- Day7/day7.py
+++ Day7/day7.py
@@ -11,11 +11,17 @@ def main():
     with open(input_file) as f:
         pos = [int(p) for p in next(f).split(',')]
 
+    #pos = [16,1,2,0,4,2,7,1,2,14]
     #puzzle1
     median_pos = median(pos)
     fuel = [abs(median_pos-x) for x in pos]
     print(sum(fuel))
 
+    #puzzle2
+    avg = sum([x/len(pos) for x in pos])
+    avg = round(avg)-1
+    fuel = [abs(avg-x)*(1+abs(avg-x))/2 for x in pos]
+    print(sum(fuel))
     return
 
 if __name__ == '__main__':
