d1b3beccb3390526a5c81eebde6399d85459c99b
bchrchwd <benj.churcheward@gmail.com>
2021-12-26 15:55:04 +0100

    Added dirty Day10#2

--- Day10/day10.py
+++ Day10/day10.py
@@ -26,6 +26,26 @@ def sum_corrupted(line):
             count += 25137
     return count
 
+def complement(c):
+    if c == '(':
+        return ')'
+    if c == '[':
+        return ']'
+    if c == '{':
+        return '}'
+    if c == '<':
+        return '>'
+
+def score_char(c):
+    if c == '(':
+        return 1
+    if c == '[':
+        return 2
+    if c == '{':
+        return 3
+    if c == '<':
+        return 4
+
 def main():
     input_file = sys.argv[1]
 
@@ -35,7 +55,7 @@ def main():
         corrupted = []
         opened = {'(', '[', '{', '<'}
         closed = {')', ']', '}', '>'}
-
+        total_score = []
         for line in lines:
             opened_char = []
             line = [l for l in line][::-1]
@@ -49,8 +69,21 @@ def main():
                         break
                     if not is_valid(opened_char.pop(), l):
                         corrupted.append(l)
+                        opened_char = []
+                        break
+            if opened_char:
+                #print(opened_char)
+                opened_char = opened_char[::-1]
+                complement_char = [complement(c) for c in opened_char]
+                print(''.join(complement_char))
+                score = 0
+                for c in opened_char:
+                    score = score*5 + score_char(c)
+                total_score.append(score)
 
     print(f"Sum of corrupted characters is {sum_corrupted(corrupted)}.")
+    middle_score = int(len(total_score)/2)
+    print(sorted(total_score)[middle_score])
 
 if __name__ == '__main__':
     main()
