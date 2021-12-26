a981d38083b093c12d1e4330b6a5a7de98d37d10
bchrchwd <benj.churcheward@gmail.com>
2021-12-26 01:21:05 +0100

    Added Day9#2

--- Day9/day9.py
+++ Day9/day9.py
@@ -2,62 +2,64 @@
 # -*- coding: utf-8 -*-
 
 import os, sys
+import math
+
+def parse_grid(input_file):
+    with open(input_file) as f:
+        #ugly, but to have only two arguments fro get_adjacents
+        global grid
+        grid = [[int(e) for e in line] for line in f.read().splitlines()]
+
+def get_adjacents(i, j):
+    adjacents = []
+    if i < len(grid[i])-1:
+        adjacents.append(grid[i+1][j])
+    if i > 0:
+        adjacents.append(grid[i-1][j])
+    if j < len(grid[i])-1:
+        adjacents.append(grid[i][j+1])
+    if j > 0:
+         adjacents.append(grid[i][j-1])
+    return adjacents
+
+def check_surface(i,j):
+    count = 0
+
+    if 0 <= i < len(grid) and 0 <= j < len(grid[i]) and grid[i][j] != 9:
+        #we don't want to count the cell again
+        grid[i][j] = 9
+        count += 1
+    else:
+        return 0
+
+    for di, dj in [(-1, 0),(1, 0),(0, 1),(0, -1)]:
+        count += check_surface(i+di, j+dj)
+
+    return count
 
 def main():
 
     input_file = sys.argv[1]
-    with open(input_file) as f:
-        lines = f.read().splitlines()
-        low_points = []
-        #lines = [(l1, l2, l3) for (l1, l2, l3) in zip(lines, lines[1:], lines[2:])]
-        for i in range(len(lines)):
-            if i == 0:
-                #first line
-                line = [int(e) for e in lines[i]]
-                line_next = [int(e) for e in lines[i+1]]
-                for j in range(len(line)):
-                    if j == 0:
-                        if line[j] < line[j+1] and line[j] < line_next[j]:
-                            low_points.append(line[j])
-                    elif j == len(line) - 1:
-                        if line[j] < line[j-1] and line[j] < line_next[j]:
-                            low_points.append(line[j])
-                    else:
-                        if line[j] < line[j+1] and line[j] < line[j-1] and line[j] < line_next[j]:
-                            low_points.append(line[j])
 
-            elif i == len(lines) - 1:
-                #last line
-                line_prev = [int(e) for e in lines[i-1]]
-                line = [int(e) for e in lines[i]]
-                for j in range(len(line)):
-                    if j == 0:
-                        if line[j] < line[j+1] and line[j] < line_prev[j]:
-                            low_points.append(line[j])
-                    elif j == len(line) - 1:
-                        if line[j] < line[j-1] and line[j] < line_prev[j]:
-                            low_points.append(line[j])
-                    else:
-                        if line[j] < line[j+1] and line[j] < line[j-1] and line[j] < line_prev[j]:
-                            low_points.append(line[j])
+    parse_grid(input_file)
+    low_points = []
 
-            else:
-                line_prev = [int(e) for e in lines[i-1]]
-                line = [int(e) for e in lines[i]]
-                line_next = [int(e) for e in lines[i+1]]
-                for j in range(len(line)):
-                    if j == 0:
-                        if line[j] < line[j+1] and line[j] < line_prev[j] and line[j] < line_next[j]:
-                            low_points.append(line[j])
-                    elif j == len(line) - 1:
-                        if line[j] < line[j-1] and line[j] < line_prev[j] and line[j] < line_next[j]:
-                            low_points.append(line[j])
-                    else:
-                        if line[j] < line[j+1] and line[j] < line[j-1] and line[j] < line_prev[j] and line[j] < line_next[j]:
-                            low_points.append(line[j])
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            adjacents = get_adjacents(i, j)
+            if grid[i][j] < min(adjacents):
+                low_points.append(grid[i][j])
+    sum_risk = sum(low_points) + len(low_points)
 
-    risk = sum(low_points) + len(low_points)
-    print(f"The sum of risks is {risk}.")
+    print(f"The sum of risks is {sum_risk}.")
+
+    #2
+    surfaces = []
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            surfaces.append(check_surface(i, j))
+    final_product = math.prod(sorted(surfaces)[-3:])
+    print(f"Product of the three largest surfaces is {final_product}.")
 
 if __name__ == '__main__':
     main()
